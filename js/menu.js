5355301f6fa89dd3d5f875aa87d5760b92f00677
kostyaslob <kostyaslob@gmail.com>
2024-12-17 22:33:37 +0200

    add js on btn shop now for closing

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,13 @@
 const menuBtnOpen = document.querySelector('.js-open-menu');
 const menuBtnClose = document.querySelector('.js-close-menu');
 const menuClose = document.querySelector('.js-link');
+const menuCloseBtn = document.querySelector('.js-btn');
 const menu = document.querySelector('.js-menu');
 
 menuBtnOpen.addEventListener('click', toggleModal);
 menuBtnClose.addEventListener('click', toggleModal);
 menuClose.addEventListener('click', toggleModal);
+menuCloseBtn.addEventListener('click', toggleModal);
 
 function toggleModal() {
 menu.classList.toggle('is-open');
