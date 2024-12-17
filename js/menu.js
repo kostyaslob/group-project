984343ea5a3b9b67fa1945d4a7346f9c6a655458
kostyaslob <kostyaslob@gmail.com>
2024-12-17 02:08:44 +0200

    upd js close function for Menu items

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,12 @@
 const menuBtnOpen = document.querySelector('.js-open-menu');
 const menuBtnClose = document.querySelector('.js-close-menu');
+const menuClose = document.querySelector('.js-link');
 const menu = document.querySelector('.js-menu');
 
 menuBtnOpen.addEventListener('click', toggleModal);
 menuBtnClose.addEventListener('click', toggleModal);
+menuClose.addEventListener('click', toggleModal);
 
 function toggleModal() {
-    menu.classList.toggle('is-open');
+menu.classList.toggle('is-open');
 }
